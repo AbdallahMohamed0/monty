afcd3f46d419ac87db23c2537dd7c611eca03485
Abdallah Mohamed <113191982+AbdallahMohamed0@users.noreply.github.com>
2023-05-25 17:58:10 +0300

    Update pint.c

--- pint.c
+++ pint.c
@@ -11,10 +11,16 @@ void f_pint(stack_t **head, unsigned int counter)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
+		exit_program(head);
 	}
+
 	printf("%d\n", (*head)->n);
+}
+
+void exit_program(stack_t **head)
+{
+	fclose(bus.file);
+	free(bus.content);
+	free_stack(*head);
+	exit(EXIT_FAILURE);
 }
