f70c350232ff060afb7276770b7883bb0cd2fd1e
Abdallah Mohamed <113191982+AbdallahMohamed0@users.noreply.github.com>
2023-05-25 16:58:53 +0300

    Done

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- function that doesn't do anything
+  *@head: double pointer to head node
+  *@counter: line_number
+  *Return: void
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
